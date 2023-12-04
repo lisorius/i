617376720d9f52789c3ae0629d7520bc3f95d397
lisorius <148572778+lisorius@users.noreply.github.com>
2023-12-04 18:08:08 +0300

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from project1.models import ModelYroslav
+
+admin.site.register(ModelYroslav)
+admin.site.register(ModelYroslav)
+admin.site.register(ModelYroslav)
